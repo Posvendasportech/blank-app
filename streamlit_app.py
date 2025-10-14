4c43da63dc966469cc7efe02cd22e87cfbb14f39
Posvendasportech <posvendasportech@gmail.com>
2025-10-14 15:38:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import os
+import streamlit as st
 
 credenciais_json = os.getenv("PLANILHA_CRM_POSVENDAS")
 
 if not credenciais_json:
-    import streamlit as st
     st.error("⚠️ A variável PLANILHA_CRM_POSVENDAS não está definida!")
 else:
-    print("Variável encontrada")
+    st.success("✅ Variável encontrada e pronta para uso")
