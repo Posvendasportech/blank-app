b35ac9ff5459c671057b36559b28bc058d3c786a
Posvendasportech <posvendasportech@gmail.com>
2025-10-14 15:32:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,9 @@
 import os
-import json
-import gspread
-from google.oauth2.service_account import Credentials
 
 credenciais_json = os.getenv("PLANILHA_CRM_POSVENDAS")
-credenciais_dict = json.loads(credenciais_json)
 
-scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_info(credenciais_dict, scopes=scope)
-client = gspread.authorize(creds)
-
-sheet = client.open("Controle de Vendas").worksheet("Base")
-dados = sheet.get_all_records()
-print(f"Planilha carregada com {len(dados)} linhas.")
+if not credenciais_json:
+    import streamlit as st
+    st.error("⚠️ A variável PLANILHA_CRM_POSVENDAS não está definida!")
+else:
+    print("Variável encontrada")
