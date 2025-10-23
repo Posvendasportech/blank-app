3131909d311a38f53e5dfa50f916dca31fc50ff8
Posvendasportech <posvendasportech@gmail.com>
2025-10-23 15:29:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.set_page_config(page_title="Dashboard de Vendas", page_icon="📊", layout="w
 # 🔗 URLs das planilhas
 # ------------------------------
 SHEET_URL_1 = "https://docs.google.com/spreadsheets/d/1d07rdyAfCzyV2go0V4CJkXd53wUmoA058WeqaHfGPBk/export?format=csv"
-SHEET_URL_2 = "https://docs.google.com/spreadsheets/d/1UD2_Q9oua4OCqYls-Is4zVKwTc9LjucLjPUgmVmyLBc/edit?usp=sharing"
+SHEET_URL_2 = "https://docs.google.com/spreadsheets/d/1UD2_Q9oua4OCqYls-Is4zVKwTc9LjucLjPUgmVmyLBc/export?format=csv"
 
 # ------------------------------
 # 🧩 Funções utilitárias
