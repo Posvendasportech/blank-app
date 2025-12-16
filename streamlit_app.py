124afd48712a8a76c834b41f4e9ce653673ca00a
Posvendasportech <posvendasportech@gmail.com>
2025-12-16 15:13:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,14 +240,14 @@ def render_checkin():
     
         with col_config2:
         # Vincular com o planejamento de metas
-        metas_por_classificacao = {
-            "Novo": meta_novo,
-            "Promissor": meta_promissor,
-            "Leal": meta_leal,
-            "Campeão": meta_campeao,
-            "Em risco": meta_risco,
-            "Dormente": meta_dormente
-        }
+            metas_por_classificacao = {
+                "Novo": meta_novo,
+                "Promissor": meta_promissor,
+                "Leal": meta_leal,
+                "Campeão": meta_campeao,
+                "Em risco": meta_risco,
+                "Dormente": meta_dormente
+            }
     
     # Pegar limite baseado na meta definida
     limite_clientes = metas_por_classificacao.get(classificacao_selecionada, 10)
